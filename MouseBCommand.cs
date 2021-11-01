4f33bd484f9ba6717ea0db77be2f941ff934f8e5
claybratt <claybratt@gmail.com>
2021-10-31 18:23:29 -0700

    Add project files.

---
+++ MouseBCommand.cs
@@ -0,0 +1,34 @@
+﻿using Rhino;
+using Rhino.Commands;
+
+
+namespace MouseB
+{
+    public class MouseBCommand : Command
+    {
+        public MouseBCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static MouseBCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "MouseBCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("MouseB");
+            return Result.Success;
+        }
+    }
+}
---
+++ MouseBPlugIn.cs
@@ -0,0 +1,52 @@
+﻿using Rhino.PlugIns;
+using System;
+
+
+namespace MouseB
+{
+    ///<summary>
+    /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    /// class. DO NOT create instances of this class yourself. It is the
+    /// responsibility of Rhino to create an instance of this class.</para>
+    /// <para>To complete plug-in information, please also see all PlugInDescription
+    /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+    /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+    ///</summary>
+    public class MouseBPlugIn : PlugIn
+
+    {
+        readonly MouseHook.MouseHook mHook = new MouseHook.MouseHook();
+
+        public MouseBPlugIn()
+        {
+            Instance = this;
+        }
+
+        ///<summary>Gets the only instance of the MouseBPlugIn plug-in.</summary>
+        public static MouseBPlugIn Instance
+        {
+            get; private set;
+        }
+
+        public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
+
+        /// <summary>
+        /// Called by Rhino when loading this plug-in.
+        /// </summary>
+        protected override LoadReturnCode OnLoad(ref string errorMessage)
+        {
+            void EventB(object sender, MouseHook.RawMouseEventArgs e) =>
+                Rhino.RhinoApp.RunScript((e.MouseData & (1 << 16)) != 0 ? "_Zoom" : "_Zoom _Target", false);
+                //Rhino.RhinoApp.WriteLine("{0}",(e.MouseData & (1 << 16)) != 0 ? "Mouse4" : "Mouse5");
+            mHook.MouseAction += new EventHandler<MouseHook.RawMouseEventArgs>(EventB);
+            mHook.Start();
+
+            Rhino.RhinoApp.WriteLine("MouseB");
+            return LoadReturnCode.Success;
+        }
+        protected override void OnShutdown()
+        {
+            mHook.Stop();
+        }
+    }
+}
